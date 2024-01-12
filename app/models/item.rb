9cd5b615e7f10af8f66b65de467063cf5ce99d88
emiri0903 <emirintosh@gmail.com>
2024-01-12 16:20:19 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :title, presence: true
-  validates :price, only_integer: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :explanation, presence: true
   validates :image, presence: true
 
