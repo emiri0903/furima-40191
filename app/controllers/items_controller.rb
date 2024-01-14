ce7b3fa73f26c2622c324e1a4333c624da6e864d
emiri0903 <emirintosh@gmail.com>
2024-01-14 09:08:38 +0900

    編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :edit]
+  before_action :set_item, only: [:edit, :update, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -21,9 +24,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:title, :condition_id, :prefecture_id, :when_will_send_id, :category_id, :price, :explanation, :postage_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  def move_to_index
+    unless @item.user == current_user
+      redirect_to action: :index
+    end
   end
 end
