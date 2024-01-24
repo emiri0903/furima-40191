d75137af4d396095c66956765691c7a9664d84ad
emiri0903 <emirintosh@gmail.com>
2024-01-24 16:22:53 +0900

    JSの改修

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,12 @@
-window.addEventListener('load', () => {
-   const priceInput = document.getElementById("item-price");
-    priceInput.addEventListener("input", () => {
-      const addTaxDom = document.getElementById("add-tax-price");
-      addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
-      const addProfitDom = document.getElementById("profit");
-      addProfitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
-  })
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+   const inputValue = parseFloat(priceInput.value);
+   const addTaxDom = document.getElementById("add-tax-price");
+   const taxValue = inputValue * 0.1;
+   const roundedTaxValue = Math.floor(taxValue);
+   addTaxDom.innerHTML = Math.floor(roundedTaxValue);
+   
+   const profitDom = document.getElementById("profit");
+   const profitValue = inputValue - roundedTaxValue;
+   profitDom.innerHTML = Math.floor(profitValue);
 });
